d39c3f6629cb87d51038229dfe955b0e1d3ec1bb
cs4jcmac <cs4jcmac@CS4-TEAM3-IMAC1.local>
2016-07-04 11:58:56 +0800

    Initial commit

---
+++ iOSChartsDemo/BarChartViewController.swift
@@ -0,0 +1,63 @@
+//
+//  BarChartViewController.swift
+//  iOSChartsDemo
+//
+//  Created by Joyce Echessa on 6/12/15.
+//  Copyright (c) 2015 Appcoda. All rights reserved.
+//
+import Charts
+import UIKit
+
+class BarChartViewController: UIViewController , ChartViewDelegate{
+
+    @IBOutlet weak var barChartView: BarChartView!
+    
+    
+    var months: [String]!	
+    @IBAction func saveChart(sender: UIBarButtonItem) {
+        barChartView.saveToCameraRoll()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        barChartView.delegate = self
+        
+        // Do any additional setup after loading the view, typically from a nib.
+        months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+        let unitsSold = [20.0, 4.0, 6.0, 3.0, 12.0, 16.0, 4.0, 18.0, 2.0, 4.0, 5.0, 4.0]
+        
+        setChart( _ : months, values: unitsSold)
+    }
+    
+    func chartValueSelected(chartView: ChartViewBase, entry: ChartDataEntry, dataSetIndex: Int, highlight: ChartHighlight) {
+        print("\(entry.value) in \(months[entry.xIndex])")
+    }
+    
+    func setChart(dataPoints: [String], values: [Double]) {
+        barChartView.noDataText = "You need to provide data for the chart."
+        var dataEntries: [BarChartDataEntry] = []
+        
+        for i in 0..<dataPoints.count {
+            let dataEntry = BarChartDataEntry(value: values[i], xIndex: i)
+            dataEntries.append(dataEntry)
+        }
+        
+        let chartDataSet = BarChartDataSet(yVals: dataEntries, label: "Units Sold")
+        let chartData = BarChartData(xVals: months, dataSet: chartDataSet)
+        chartDataSet.colors = [UIColor(red: CGFloat(Double(arc4random_uniform(256)/255)), green: CGFloat(Double(arc4random_uniform(256)/255)), blue: CGFloat(Double(arc4random_uniform(256))/255), alpha: 1)]
+        chartDataSet.colors = ChartColorTemplates.colorful()
+        barChartView.data = chartData
+        barChartView.xAxis.labelPosition = .BothSided
+        barChartView.descriptionText = ""
+        barChartView.backgroundColor = UIColor(red: 189/255, green: 195/255, blue: 199/255, alpha: 1)
+//        barChartView.backgroundColor = UIColor(red: CGFloat(Double(arc4random_uniform(255)/255)), green: CGFloat(Double(arc4random_uniform(255)/255)), blue: CGFloat(Double(arc4random_uniform(255))/255), alpha: 1)
+        barChartView.animate(xAxisDuration: 2.0, yAxisDuration: 2.0, easingOption: .EaseInOutQuad)
+        let ll = ChartLimitLine(limit: 10.0, label: "Target")
+        barChartView.rightAxis.addLimitLine(ll)
+        
+    }
+
+
+}
+
